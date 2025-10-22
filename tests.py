2f3a23f1490b06aa316d94d77df266a301880388
SERGEY <serhf3dorov@yandex.ru>
2025-10-22 22:04:38 +0300

    Sprint_4

--- tests.py
+++ tests.py
@@ -51,47 +51,63 @@ class TestBooksCollector:
         assert collector.get_books_genre().get('Солярис') == ''
 
     # get_book_genre
-    def test_get_book_genre_for_book_from_books_genre_shows_book_genre(self, collector, collector_with_books_and_genres):
+    def test_get_book_genre_for_book_from_books_genre_shows_book_genre(self, collector):
+        collector.add_new_book('Солярис')
+        collector.set_book_genre('Солярис', 'Фантастика')
         assert collector.get_book_genre('Солярис') == 'Фантастика'
 
     # get_books_with_specific_genre
-    def test_get_books_with_specific_genre_for_books_from_books_genre_shows_two_books(self, collector, collector_with_books_and_genres):
-        assert collector.get_books_with_specific_genre('Ужасы') == ['Оно', 'Пятачок в угаре']
+    def test_get_books_with_specific_genre_for_books_from_books_genre_shows_two_books(self, collector):
+        collector.add_new_book('Солярис')
+        collector.set_book_genre('Солярис', 'Фантастика')
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Солярис']
 
-    def test_get_books_with_specific_genre_book_genre_not_from_genre_list_shows_none(self, collector, collector_with_books_and_genres):
+    def test_get_books_with_specific_genre_book_genre_not_from_genre_list_shows_none(self, collector):
+        collector.add_new_book('Солярис')
+        collector.set_book_genre('Солярис', 'Фантастика')
         assert collector.get_books_with_specific_genre('Боевик') == []
 
     # get_books_genre
-    def test_get_books_genre_shows_list_of_books_with_genres(self, collector, collector_with_books_and_genres):
-        books = {'Солярис': 'Фантастика', 'Оно': 'Ужасы', 'Пуаро': 'Детективы', 'Моана': 'Мультфильмы', 'Ревизор': 'Комедии', 'Пятачок в угаре': 'Ужасы'}
+    def test_get_books_genre_shows_list_of_books_with_genres(self, collector):
+        collector.add_new_book('Солярис')
+        collector.set_book_genre('Солярис', 'Фантастика')
+        books = {'Солярис': 'Фантастика'}
         assert collector.get_books_genre() == books
 
     # get_books_for_children
-    def test_get_books_for_children_shows_books_with_no_age_rating(self, collector, collector_with_books_and_genres):
-        books_for_children = ['Солярис', 'Моана', 'Ревизор']
+    def test_get_books_for_children_shows_books_with_no_age_rating(self, collector):
+        collector.add_new_book('Солярис')
+        collector.set_book_genre('Солярис', 'Фантастика')
+        books_for_children = ['Солярис']
         assert collector.get_books_for_children() == books_for_children
 
     # add_book_in_favorites
-    def test_add_book_in_favorites_book_from_books_genre_book_added(self, collector, collector_with_books_and_genres):
+    def test_add_book_in_favorites_book_from_books_genre_book_added(self, collector):
+        collector.add_new_book('Солярис')
         collector.add_book_in_favorites('Солярис')
         assert collector.get_list_of_favorites_books() == ['Солярис']
 
-    def test_add_book_in_favorites_book_not_from_books_genre_book_not_added(self, collector, collector_with_books_and_genres):
+    def test_add_book_in_favorites_book_not_from_books_genre_book_not_added(self, collector):
         collector.add_book_in_favorites('Капитан Немо')
         assert len(collector.get_list_of_favorites_books()) == 0
 
-    def test_add_book_in_favorites_add_same_book_three_times_book_added_once(self, collector, collector_with_books_and_genres):
+    def test_add_book_in_favorites_add_same_book_three_times_book_added_once(self, collector):
+        collector.add_new_book('Солярис')
         collector.add_book_in_favorites('Солярис')
         collector.add_book_in_favorites('Солярис')
         collector.add_book_in_favorites('Солярис')
         assert len(collector.get_list_of_favorites_books()) == 1
 
     # delete_book_from_favorites
-    def test_delete_book_from_favorites_delete_one_book_book_deleted(self, favorites, collector, collector_with_books_and_genres):
+    def test_delete_book_from_favorites_delete_one_book_book_deleted(self, collector):
+        collector.add_new_book('Солярис')
+        collector.add_book_in_favorites('Солярис')
         collector.delete_book_from_favorites('Солярис')
         assert 'Солярис' not in collector.get_list_of_favorites_books()
 
     # get_list_of_favorites_books
-    def test_get_list_of_favorites_books_shows_list_of_favorite_books(self, favorites, collector, collector_with_books_and_genres):
-        favorites =['Солярис','Оно', 'Пуаро', 'Моана', 'Ревизор', 'Пятачок в угаре']
+    def test_get_list_of_favorites_books_shows_list_of_favorite_books(self, collector):
+        collector.add_new_book('Солярис')
+        collector.add_book_in_favorites('Солярис')
+        favorites =['Солярис']
         assert collector.get_list_of_favorites_books() == favorites
